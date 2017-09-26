21a3e71468e67dc6a60f72618bcba9cd4fb54296
ionut-plesca <32291148+ionut-plesca@users.noreply.github.com>
2017-09-26 19:56:54 +0300

    Add files via upload

---
+++ deutche-master/deutche-master/src/main/java/ro/deutsche/mediastore/controller/WelcomeController.java
@@ -0,0 +1,41 @@
+package ro.deutsche.mediastore.controller;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static ro.deutsche.mediastore.domain.AgeAudiance.CHILDREN;
+import ro.deutsche.mediastore.domain.Client;
+import ro.deutsche.mediastore.domain.CustomerCategory;
+import ro.deutsche.mediastore.domain.FrequesntReleasePoints;
+import static ro.deutsche.mediastore.domain.FrequesntReleasePoints.NEW_RELEASE;
+import ro.deutsche.mediastore.domain.Rental;
+import ro.deutsche.mediastore.service.ClientService;
+
+@Controller
+public class WelcomeController {
+	
+	@Autowired
+	ClientService clientService;
+		
+	@RequestMapping(value="/")
+	public String welcome(Map<String, Object> model) {
+		Client client = clientService.getSoleClient();
+		String report = getCartStatement(client);
+		model.put("report", report);
+		return "welcome";
+	}
+
+	public String getCartStatement(Client client) {
+		StringBuilder report = new StringBuilder("report for " + client.getName()+"\n");
+		for(Rental rental: client.getRentals()) {
+			report.append(rental.getProduct().getName()+", ");
+			System.out.println(rental.getProduct().getName());
+		}
+		return report.substring(0, report.length()-2).toString();
+	}
+
+}
